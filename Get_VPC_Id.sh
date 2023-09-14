c3c9fd504880c03bd14f918b06be0d061bf8fff3
Lenin Akurati <leninakurati@lenins-air.home>
2023-09-14 19:01:01 +0100

    newshellcode

--- Get_VPC_Id.sh
+++ Get_VPC_Id.sh
@@ -1,1 +1,6 @@
-dcdsfegrgerger
+#!/bin/bash
+VPCID=$(aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r)
+for vpc in $VPCId; do
+    echo "The VPC ID is:$VPC"
+    echo "--------------"
+done
