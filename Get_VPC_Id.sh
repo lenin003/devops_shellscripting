e1c9ead5b461264f55f08e07a698b1501046494d
Lenin Akurati <leninakurati@lenins-air.home>
2023-10-17 22:55:14 +0100

    updated for loop

--- Get_VPC_Id.sh
+++ Get_VPC_Id.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VPCID=$(aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r)
-for vpc in $VPCId; do
+for vpc in $VPCID; do
     echo "The VPC ID is:$VPC"
     echo "--------------"
     echo "**************"
