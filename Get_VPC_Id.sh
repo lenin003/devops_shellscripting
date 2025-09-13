dbaa9cacfea4ba17424c6981a05cddc22f38a12e
Lenin Akurati <leninakurati@mac.home>
2025-09-13 21:23:54 +0100

    extra echo

--- Get_VPC_Id.sh
+++ Get_VPC_Id.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 VPCID=$(aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r)
 for vpc in $VPCID; do
+    echo "extra echo"
     echo "The VPC ID is:$VPC"
     echo "--------------"
     echo "**************"
+    echo "only this beanch"    cp get_vpc_id.sh {destination_path}
 done
