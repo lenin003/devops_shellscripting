4710ab00e3e06405a7c14f14bca808efd3a48557
Lenin Akurati <leninakurati@mac.home>
2025-11-01 21:00:30 +0000

    special variables adding all statuses

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,2 +1,11 @@
 #!bin/bash
 echo "all variables passed to the script: $@"
+echo "number of variables passed to the script: $#"
+echo "script name is : $0"
+echo "current directory : $PWD"
+echo "user running the script : $USER"
+echo "home directory of the user : $HOME"
+echo "process id of current script is : $$"
+sleep 10 &
+echo "background process id : $!"
+echo "last command exit status : $?"            
